a5fd5fc636f2f09f85b8308fe84df0f4c0c4e347
Tanvir Ahmed <ahmedtanvir290@gmail.com>
2022-05-09 21:59:14 +0600

    data send to client side

--- index.js
+++ index.js
@@ -14,14 +14,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fcoza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('databese connected')
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('prefume-warehouse').collection('perfumes');
 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
