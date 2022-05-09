d15f9f2f0c04300bf797dba7185d34f34a6fe9a1
Tanvir Ahmed <ahmedtanvir290@gmail.com>
2022-05-09 20:20:00 +0600

    DB username and password sequre

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const res = require('express/lib/response');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fcoza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('databese connected')
+    client.close();
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hurryyyyyyyyyyyyyyyyyyyyy')
